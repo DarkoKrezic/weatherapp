2f7fbf7b84f2256bfe3eb4c55115b618f24dead7
Henrike Stahlhut <henrike.st@icloud.com>
2023-04-13 15:10:18 +0200

    add and finish filter for list

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,14 +1,21 @@
 import React from "react";
 
-export default function List({ activities }) {
+export default function List({ activities, isGoodWeather }) {
   return (
-    <ul>
-      {activities.map((activity) => (
-        <li key={activity.id}>
-          {activity.name} -{" "}
-          {activity.isForGoodWeather ? "good weather" : "bad weather"}
-        </li>
-      ))}
-    </ul>
+    <>
+      <h2>
+        {isGoodWeather
+          ? "The Weather is awesome! Good Weather Activities:"
+          : "Bad Weather :( You can do:"}
+      </h2>
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>
+            {activity.name} -{" "}
+            {activity.isForGoodWeather ? "good weather" : "bad weather"}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
