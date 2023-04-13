abc975a95eb46ac7170c60eeb4ea528bf1c6d943
DarkoKrezic <darkotronic@gmail.com>
2023-04-13 12:42:11 +0200

    form fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import "./App.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Form from "./components/Form/Form";
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  function handleAddActivity(newActivity) {
+    const id = uuidv4();
+    const activityWithId = { ...newActivity, id };
+    setActivities([...activities, activityWithId]);
+  }
+
   return (
-    <div className="App">
-      <Form />
+    <div>
+      <Form onAddActivity={handleAddActivity} />
+      <ul>
+        {activities.map((activity) => (
+          <li key={activity.id}>
+            {activity.name} -{" "}
+            {activity.isForGoodWeather ? "good weather" : "bad weather"}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
